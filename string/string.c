feff6a91811a976eb954a4b94bae1029141e0234
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-08-06 22:07:23 -0500

    Initial version of string manipulation funcs.
    This is a simple program that contains the definition of a string data
    type, and some helper functions to manipulate that data type.

---
+++ string/string.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct string {
+  char *data;
+  int len;
+} string;
+
+string *string_initialize() {
+  string *s = malloc(sizeof(string));
+  s->data = NULL;
+  s->len = 0;
+  return s;
+}
+
+char *string_copy(char *dest, const char *src) {
+  for (int i = 0;; i++) {
+    dest[i] = src[i];
+    if (!src[i]) {
+      break;
+    }
+  }
+}
+
+size_t string_length(const char *s) {
+  size_t len = 0;
+  for (;; len++) {
+    if (!s[len]) {
+      break;
+    }
+  }
+  return len;
+}
+
+void string_set(string *s, char *str) {
+  int len = string_length(str);
+  s->data = malloc(len);
+  s->len = len;
+  string_copy(s->data, str);
+}
+
+int main() {
+  string *s = string_initialize();
+  string_set(s, "Hello, World!");
+  puts(s->data);
+}
