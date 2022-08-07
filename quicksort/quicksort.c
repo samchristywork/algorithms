a3845f9f16fe939bc75a0d71b6ba3dcaaf6c2b87
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-08-07 18:14:56 -0500

    Added comments.

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
+/*
+ * Macro for swapping the value of two variables
+ */
 #define SWAP(x, y) \
   int t = x;       \
   x = y;           \
   y = t;
 
+/*
+ * The canonical comparison function for two integers.
+ */
 int compare(int a, int b) {
   return a < b;
 }
 
+/*
+ * A comparison function that takes into account the absolute value of the
+ * operands.
+ */
 int abs_compare(int a, int b) {
   if (a < 0)
     a *= -1;
@@ -17,11 +27,22 @@ int abs_compare(int a, int b) {
   return a < b;
 }
 
+/*
+ * The main sorting algorithm. Takes the array to be sorted, the array's
+ * length, and a comparison function.
+ */
 void quicksort(int *arr, int len, int (*compare)(int, int)) {
+
+  /*
+   * 0-element and 1-element arrays are by definition already sorted.
+   */
   if (len < 2) {
     return;
   }
 
+  /*
+   * The "meat" of the algorithm.
+   */
   int l = 0;
   int r = len - 1;
   while (1) {
@@ -42,10 +63,16 @@ void quicksort(int *arr, int len, int (*compare)(int, int)) {
     r--;
   }
 
+  /*
+   * Recurse using the "left" and "right" sides of the array.
+   */
   quicksort(arr, l, compare);
   quicksort(arr + l, len - l, compare);
 }
 
+/*
+ * A convenience function for printing out an array.
+ */
 void print_array(int *arr, int len) {
   for (int i = 0; i < len; i++) {
     printf("%d ", arr[i]);
