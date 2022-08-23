9223f1b3af0313faf36a1f807af0077343c9ce84
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-08-22 23:33:08 -0500

    Added comments.

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * The data structure that each element of the linked list consists of.
+ */
 typedef struct linked_list_node {
   int i;
   struct linked_list_node *child;
 } linked_list_node;
 
+/*
+ * The wrapper that makes up a whole linked list.
+ */
 typedef struct linked_list {
   linked_list_node *head;
   linked_list_node *tail;
 } linked_list;
 
+/*
+ * Add an element to the end of a linked list.
+ */
 void linked_list_append(linked_list *l, int i) {
   linked_list_node *n = malloc(sizeof(linked_list_node));
   n->child = NULL;
@@ -19,6 +28,9 @@ void linked_list_append(linked_list *l, int i) {
   l->tail = n;
 }
 
+/*
+ * Print out the contents of a linked list.
+ */
 void linked_list_print(linked_list_node *n) {
   if (!n) {
     return;
@@ -27,6 +39,9 @@ void linked_list_print(linked_list_node *n) {
   linked_list_print(n->child);
 }
 
+/*
+ * Free every element in a linked list.
+ */
 void free_all(linked_list_node *n) {
   if (!n) {
     return;
@@ -35,7 +50,10 @@ void free_all(linked_list_node *n) {
   free(n);
 }
 
-linked_list *linked_list_init(){
+/*
+ * Initialize a linked list struct.
+ */
+linked_list *linked_list_init() {
   linked_list_node *n = malloc(sizeof(linked_list_node));
   n->child = NULL;
   n->i = 0;
@@ -47,13 +65,23 @@ linked_list *linked_list_init(){
 }
 
 int main() {
-  linked_list *l=linked_list_init();
+  linked_list *l = linked_list_init();
 
+  /*
+   * Add ten elements to the linked list.
+   */
   for (int i = 0; i < 10; i++) {
     linked_list_append(l, i);
   }
 
+  /*
+   * Print out the linked list.
+   */
   linked_list_print(l->head->child);
 
+  /*
+   * Cleanup.
+   */
   free_all(l->head);
+  free(l);
 }
