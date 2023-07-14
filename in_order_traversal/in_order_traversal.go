3560b3a32d680f3a2ee22e7af29444257e9c13af
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-13 21:29:10 -0500

    feat: Add callback function
    This allows me to print the values as we traverse the tree, instead of
    just returning an array of numbers, we can do something as we traverse.

--- in_order_traversal/in_order_traversal.go
+++ in_order_traversal/in_order_traversal.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
 )
 
 type Node struct {
@@ -9,17 +10,22 @@ type Node struct {
   Right *Node
 }
 
-func in_order_traversal(tree *Node) []int {
+func print_element(value int) {
+  fmt.Println(value)
+}
+
+func in_order_traversal(tree *Node, callback func(int)) []int {
   var values []int
 
   if tree.Left != nil {
-    values = append(values, in_order_traversal(tree.Left)...)
+    values = append(values, in_order_traversal(tree.Left, callback)...)
   }
 
   values = append(values, tree.Value)
+  callback(tree.Value, depth)
 
   if tree.Right != nil {
-    values = append(values, in_order_traversal(tree.Right)...)
+    values = append(values, in_order_traversal(tree.Right, callback)...)
   }
 
   return values
