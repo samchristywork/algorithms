18574ff611e5e9aa560e969881c89e8ed40e4d44
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-13 21:30:42 -0500

    feat: Add depth so we can pretty-print the tree
    The tree looks better if we indent each element based on how far away
    from the root it is.

--- in_order_traversal/in_order_traversal.go
+++ in_order_traversal/in_order_traversal.go
@@ -10,22 +10,25 @@ type Node struct {
   Right *Node
 }
 
-func print_element(value int) {
+func print_element(value int, depth int) {
+  for i := 0; i < depth; i++ {
+    fmt.Print("  ")
+  }
   fmt.Println(value)
 }
 
-func in_order_traversal(tree *Node, callback func(int)) []int {
+func in_order_traversal(tree *Node, callback func(int, int), depth int) []int {
   var values []int
 
   if tree.Left != nil {
-    values = append(values, in_order_traversal(tree.Left, callback)...)
+    values = append(values, in_order_traversal(tree.Left, callback, depth+1)...)
   }
 
   values = append(values, tree.Value)
   callback(tree.Value, depth)
 
   if tree.Right != nil {
-    values = append(values, in_order_traversal(tree.Right, callback)...)
+    values = append(values, in_order_traversal(tree.Right, callback, depth+1)...)
   }
 
   return values
