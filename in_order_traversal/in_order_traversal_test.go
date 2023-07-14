540cbe852c35736e3e00d3d1fac774e9e2cb48dc
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-13 21:32:55 -0500

    test: Add a simple general test of the algorithm
    This test ensures that the algorithm can correctly handle a simple tree.

---
+++ in_order_traversal/in_order_traversal_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "testing"
+)
+
+func TestGeneral(t *testing.T) {
+  tree := &Node{Value: 1}
+  tree.Left = &Node{Value: 2}
+  tree.Right = &Node{Value: 3}
+  tree.Left.Left = &Node{Value: 4}
+  tree.Left.Right = &Node{Value: 5}
+  tree.Right.Left = &Node{Value: 6}
+  tree.Right.Right = &Node{Value: 7}
+  tree.Left.Left.Left = &Node{Value: 8}
+  tree.Left.Left.Right = &Node{Value: 9}
+  tree.Left.Right.Left = &Node{Value: 10}
+  tree.Left.Right.Right = &Node{Value: 11}
+  tree.Right.Left.Left = &Node{Value: 12}
+
+  expected := []int{8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 3, 7}
+  actual := in_order_traversal(tree, print_element)
+
+  if len(expected) != len(actual) {
+    t.Errorf("Expected %v, got %v", expected, actual)
+  }
+
+  for i, v := range expected {
+    if v != actual[i] {
+      t.Errorf("Expected %v, got %v", expected, actual)
+    }
+  }
+}
