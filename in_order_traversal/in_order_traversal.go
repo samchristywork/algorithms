2d31edcddefc747dee0e107f336dbe36e2be6aee
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-13 21:31:24 -0500

    feat: Add wrapper function
    This wrapper function makes it so that the caller doesn't have to
    specify the depth, which should always start at 0.

--- in_order_traversal/in_order_traversal.go
+++ in_order_traversal/in_order_traversal.go
@@ -17,19 +17,23 @@ func print_element(value int, depth int) {
   fmt.Println(value)
 }
 
-func in_order_traversal(tree *Node, callback func(int, int), depth int) []int {
+func _in_order_traversal(tree *Node, callback func(int, int), depth int) []int {
   var values []int
 
   if tree.Left != nil {
-    values = append(values, in_order_traversal(tree.Left, callback, depth+1)...)
+    values = append(values, _in_order_traversal(tree.Left, callback, depth+1)...)
   }
 
   values = append(values, tree.Value)
   callback(tree.Value, depth)
 
   if tree.Right != nil {
-    values = append(values, in_order_traversal(tree.Right, callback, depth+1)...)
+    values = append(values, _in_order_traversal(tree.Right, callback, depth+1)...)
   }
 
   return values
+}
+
+func in_order_traversal(tree *Node, callback func(int, int)) []int {
+  return _in_order_traversal(tree, callback, 0)
 }
