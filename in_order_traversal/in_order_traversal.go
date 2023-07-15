b604a67761c97a3b8b38a56d124aa1fa6c27f8e6
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-14 19:15:02 -0500

    style: Ran `go fmt`
    As much as I don't like that Golang uses tabs, I'm going to stick to
    their style conventions.

--- in_order_traversal/in_order_traversal.go
+++ in_order_traversal/in_order_traversal.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 type Node[T interface{}] struct {
-  Value T
-  Left *Node[T]
-  Right *Node[T]
+	Value T
+	Left  *Node[T]
+	Right *Node[T]
 }
 
 func print_element[T any](value T, depth int) {
-  for i := 0; i < depth; i++ {
-    fmt.Print("  ")
-  }
-  fmt.Println(value)
+	for i := 0; i < depth; i++ {
+		fmt.Print("  ")
+	}
+	fmt.Println(value)
 }
 
 func _in_order_traversal[T any](tree *Node[T], callback func(T, int), depth int) []T {
-  var values []T
+	var values []T
 
-  if tree.Left != nil {
-    values = append(values, _in_order_traversal(tree.Left, callback, depth+1)...)
-  }
+	if tree.Left != nil {
+		values = append(values, _in_order_traversal(tree.Left, callback, depth+1)...)
+	}
 
-  values = append(values, tree.Value)
-  if callback != nil {
-    callback(tree.Value, depth)
-  }
+	values = append(values, tree.Value)
+	if callback != nil {
+		callback(tree.Value, depth)
+	}
 
-  if tree.Right != nil {
-    values = append(values, _in_order_traversal(tree.Right, callback, depth+1)...)
-  }
+	if tree.Right != nil {
+		values = append(values, _in_order_traversal(tree.Right, callback, depth+1)...)
+	}
 
-  return values
+	return values
 }
 
 func in_order_traversal(tree *Node[int], callback func(int, int)) []int {
-  if tree == nil {
-    return []int{}
-  }
+	if tree == nil {
+		return []int{}
+	}
 
-  return _in_order_traversal(tree, callback, 0)
+	return _in_order_traversal(tree, callback, 0)
 }
--- in_order_traversal/in_order_traversal_test.go
+++ in_order_traversal/in_order_traversal_test.go
@@ -1,105 +1,105 @@
 package main
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestGeneral(t *testing.T) {
-  tree := &Node[int]{Value: 1}
-  tree.Left = &Node[int]{Value: 2}
-  tree.Right = &Node[int]{Value: 3}
-  tree.Left.Left = &Node[int]{Value: 4}
-  tree.Left.Right = &Node[int]{Value: 5}
-  tree.Right.Left = &Node[int]{Value: 6}
-  tree.Right.Right = &Node[int]{Value: 7}
-  tree.Left.Left.Left = &Node[int]{Value: 8}
-  tree.Left.Left.Right = &Node[int]{Value: 9}
-  tree.Left.Right.Left = &Node[int]{Value: 10}
-  tree.Left.Right.Right = &Node[int]{Value: 11}
-  tree.Right.Left.Left = &Node[int]{Value: 12}
+	tree := &Node[int]{Value: 1}
+	tree.Left = &Node[int]{Value: 2}
+	tree.Right = &Node[int]{Value: 3}
+	tree.Left.Left = &Node[int]{Value: 4}
+	tree.Left.Right = &Node[int]{Value: 5}
+	tree.Right.Left = &Node[int]{Value: 6}
+	tree.Right.Right = &Node[int]{Value: 7}
+	tree.Left.Left.Left = &Node[int]{Value: 8}
+	tree.Left.Left.Right = &Node[int]{Value: 9}
+	tree.Left.Right.Left = &Node[int]{Value: 10}
+	tree.Left.Right.Right = &Node[int]{Value: 11}
+	tree.Right.Left.Left = &Node[int]{Value: 12}
 
-  expected := []int{8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 3, 7}
-  actual := in_order_traversal(tree, print_element[int])
+	expected := []int{8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 3, 7}
+	actual := in_order_traversal(tree, print_element[int])
 
-  if len(expected) != len(actual) {
-    t.Errorf("Expected %v, got %v", expected, actual)
-  }
+	if len(expected) != len(actual) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
 
-  for i, v := range expected {
-    if v != actual[i] {
-      t.Errorf("Expected %v, got %v", expected, actual)
-    }
-  }
+	for i, v := range expected {
+		if v != actual[i] {
+			t.Errorf("Expected %v, got %v", expected, actual)
+		}
+	}
 }
 
 func TestEmpty(t *testing.T) {
-  tree := &Node[int]{Value: 1}
+	tree := &Node[int]{Value: 1}
 
-  expected := []int{1}
-  actual := in_order_traversal(tree, print_element[int])
+	expected := []int{1}
+	actual := in_order_traversal(tree, print_element[int])
 
-  if len(expected) != len(actual) {
-    t.Errorf("Expected %v, got %v", expected, actual)
-  }
+	if len(expected) != len(actual) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
 
-  for i, v := range expected {
-    if v != actual[i] {
-      t.Errorf("Expected %v, got %v", expected, actual)
-    }
-  }
+	for i, v := range expected {
+		if v != actual[i] {
+			t.Errorf("Expected %v, got %v", expected, actual)
+		}
+	}
 }
 
 func TestLeftOnly(t *testing.T) {
-  tree := &Node[int]{Value: 1}
-  tree.Left = &Node[int]{Value: 2}
-  tree.Left.Left = &Node[int]{Value: 3}
-  tree.Left.Left.Left = &Node[int]{Value: 4}
-  tree.Left.Left.Left.Left = &Node[int]{Value: 5}
+	tree := &Node[int]{Value: 1}
+	tree.Left = &Node[int]{Value: 2}
+	tree.Left.Left = &Node[int]{Value: 3}
+	tree.Left.Left.Left = &Node[int]{Value: 4}
+	tree.Left.Left.Left.Left = &Node[int]{Value: 5}
 
-  expected := []int{5, 4, 3, 2, 1}
-  actual := in_order_traversal(tree, print_element[int])
+	expected := []int{5, 4, 3, 2, 1}
+	actual := in_order_traversal(tree, print_element[int])
 
-  if len(expected) != len(actual) {
-    t.Errorf("Expected %v, got %v", expected, actual)
-  }
+	if len(expected) != len(actual) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
 
-  for i, v := range expected {
-    if v != actual[i] {
-      t.Errorf("Expected %v, got %v", expected, actual)
-    }
-  }
+	for i, v := range expected {
+		if v != actual[i] {
+			t.Errorf("Expected %v, got %v", expected, actual)
+		}
+	}
 }
 
 func TestNil(t *testing.T) {
-  expected := []int{}
-  actual := in_order_traversal(nil, print_element[int])
+	expected := []int{}
+	actual := in_order_traversal(nil, print_element[int])
 
-  if len(expected) != len(actual) {
-    t.Errorf("Expected %v, got %v", expected, actual)
-  }
+	if len(expected) != len(actual) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
 
-  for i, v := range expected {
-    if v != actual[i] {
-      t.Errorf("Expected %v, got %v", expected, actual)
-    }
-  }
+	for i, v := range expected {
+		if v != actual[i] {
+			t.Errorf("Expected %v, got %v", expected, actual)
+		}
+	}
 
-  tree := &Node[int]{Value: 1}
-  tree.Left = &Node[int]{Value: 2}
-  tree.Left.Left = &Node[int]{Value: 3}
-  tree.Left.Left.Left = &Node[int]{Value: 4}
-  tree.Left.Left.Left.Left = &Node[int]{Value: 5}
+	tree := &Node[int]{Value: 1}
+	tree.Left = &Node[int]{Value: 2}
+	tree.Left.Left = &Node[int]{Value: 3}
+	tree.Left.Left.Left = &Node[int]{Value: 4}
+	tree.Left.Left.Left.Left = &Node[int]{Value: 5}
 
-  expected = []int{5, 4, 3, 2, 1}
-  actual = in_order_traversal(tree, nil)
+	expected = []int{5, 4, 3, 2, 1}
+	actual = in_order_traversal(tree, nil)
 
-  if len(expected) != len(actual) {
-    t.Errorf("Expected %v, got %v", expected, actual)
-  }
+	if len(expected) != len(actual) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
 
-  for i, v := range expected {
-    if v != actual[i] {
-      t.Errorf("Expected %v, got %v", expected, actual)
-    }
-  }
+	for i, v := range expected {
+		if v != actual[i] {
+			t.Errorf("Expected %v, got %v", expected, actual)
+		}
+	}
 }
