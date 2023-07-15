41e4881fd61a4ffdf5745d748b7c100c3eaa3aed
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-14 19:24:01 -0500

    test: Add general operations test
    This test runs the algorithm through its paces and makes sure no smoke
    comes out.

---
+++ direct_address_table/direct_address_table_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGeneral(t *testing.T) {
+	table := DirectAddressTable[int]{}
+	table.Insert("foo", 1)
+	table.Insert("bar", 20)
+	table.Insert("baz", 30)
+	table.Insert("qux", 40)
+	table.Insert("quux", 50)
+	table.Insert("corge", 60)
+	table.Insert("grault", 70)
+	table.Insert("garply", 80)
+	table.Insert("waldo", 90)
+	table.Insert("fred", 100)
+	table.Insert("plugh", 110)
+
+	table.Delete("grault")
+	table.Delete("qux")
+	table.Delete("bar")
+
+	foo := table.Find("foo")
+	if *foo != 1 {
+		t.Errorf("Expected 1, got %d", *foo)
+	}
+
+	faz := table.Find("faz")
+	if faz != nil {
+		t.Errorf("Expected nil, got %d", *faz)
+	}
+
+	grault := table.Find("grault")
+	if grault != nil {
+		t.Errorf("Expected nil, got %d", *grault)
+	}
+
+	for i := 0; i < MAX_KEY; i++ {
+		fmt.Printf("%d: %v\n", i, table.table[i])
+	}
+
+	length := table.Len()
+	if length != 8 {
+		t.Errorf("Expected 8, got %d", length)
+	}
+}
