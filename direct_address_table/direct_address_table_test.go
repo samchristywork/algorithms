199e0617b9fa15fb9be4805f9db2279251ac6742
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-14 19:24:53 -0500

    test: Add basic functional tests
    These tests test the insert function, generics, and the empty set case.
    
    More tests are necessary.

--- direct_address_table/direct_address_table_test.go
+++ direct_address_table/direct_address_table_test.go
@@ -47,3 +47,73 @@ func TestGeneral(t *testing.T) {
 		t.Errorf("Expected 8, got %d", length)
 	}
 }
+
+func TestInsert(t *testing.T) {
+	table := DirectAddressTable[int]{}
+	table.Insert("foo", 1)
+	table.Insert("bar", 20)
+	table.Insert("baz", 30)
+	table.Insert("qux", 40)
+	table.Insert("quux", 50)
+	table.Insert("corge", 60)
+	table.Insert("grault", 70)
+	table.Insert("garply", 80)
+	table.Insert("waldo", 90)
+	table.Insert("fred", 100)
+	table.Insert("plugh", 110)
+
+	foo := table.Find("foo")
+	if *foo != 1 {
+		t.Errorf("Expected 1, got %d", *foo)
+	}
+
+	faz := table.Find("faz")
+	if faz != nil {
+		t.Errorf("Expected nil, got %d", *faz)
+	}
+
+	grault := table.Find("grault")
+	if *grault != 70 {
+		t.Errorf("Expected 70, got %d", *grault)
+	}
+
+	for i := 0; i < MAX_KEY; i++ {
+		fmt.Printf("%d: %v\n", i, table.table[i])
+	}
+
+	length := table.Len()
+	if length != 11 {
+		t.Errorf("Expected 11, got %d", length)
+	}
+}
+
+func TestFloat(t *testing.T) {
+	table := DirectAddressTable[float64]{}
+	table.Insert("foo", 1.0)
+	table.Insert("bar", 20.0)
+	table.Insert("baz", 30.0)
+
+	foo := table.Find("foo")
+	if *foo != 1.0 {
+		t.Errorf("Expected 1.0, got %f", *foo)
+	}
+
+	faz := table.Find("faz")
+	if faz != nil {
+		t.Errorf("Expected nil, got %f", *faz)
+	}
+}
+
+func TestEmpty(t *testing.T) {
+	table := DirectAddressTable[float64]{}
+
+	foo := table.Find("foo")
+	if foo != nil {
+		t.Errorf("Expected nil, got %f", *foo)
+	}
+
+	length := table.Len()
+	if length != 0 {
+		t.Errorf("Expected 0, got %d", length)
+	}
+}
