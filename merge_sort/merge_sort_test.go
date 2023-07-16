7722e2463e662ed2b4fc5bbae28eb2f103f5f56d
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-15 20:03:54 -0500

    test: Add remaining tests
    These are some tests for edge cases, including empty and small lists,
    and other data types.

--- merge_sort/merge_sort_test.go
+++ merge_sort/merge_sort_test.go
@@ -37,3 +37,55 @@ func TestLotsOfData(t *testing.T) {
 		}
 	}
 }
+
+func TestEmpty(t *testing.T) {
+	data := []int{}
+	expected := []int{}
+
+	actual := merge_sort(data)
+
+	for i, v := range actual {
+		if v != expected[i] {
+			t.Errorf("Expected %d, got %d", expected[i], v)
+		}
+	}
+}
+
+func TestSingle(t *testing.T) {
+	data := []int{1}
+	expected := []int{1}
+
+	actual := merge_sort(data)
+
+	for i, v := range actual {
+		if v != expected[i] {
+			t.Errorf("Expected %d, got %d", expected[i], v)
+		}
+	}
+}
+
+func TestFloats(t *testing.T) {
+	data := []float64{1.0, 4.0, 2.0, 5.0, 3.0}
+	expected := []float64{1.0, 2.0, 3.0, 4.0, 5.0}
+
+	actual := merge_sort(data)
+
+	for i, v := range actual {
+		if v != expected[i] {
+			t.Errorf("Expected %f, got %f", expected[i], v)
+		}
+	}
+}
+
+func TestStrings(t *testing.T) {
+	data := []string{"foo", "bar", "baz"}
+	expected := []string{"bar", "baz", "foo"}
+
+	actual := merge_sort(data)
+
+	for i, v := range actual {
+		if v != expected[i] {
+			t.Errorf("Expected %s, got %s", expected[i], v)
+		}
+	}
+}
