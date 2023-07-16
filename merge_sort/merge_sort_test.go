dc9dd3d4a8c757ae7b0caacc2b5881416b581fb0
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-15 20:01:42 -0500

    test: Add some basic tests
    The general test simply provides hard-coded data. The "Lots of Data"
    test generates data at runtime. There is an argument to be made that you
    shouldn't use random data for these tests, but whatever.

--- merge_sort/merge_sort_test.go
+++ merge_sort/merge_sort_test.go
@@ -1,8 +1,39 @@
 package main
 
 import (
+	"math/rand"
+	"sort"
 	"testing"
 )
 
 func TestGeneral(t *testing.T) {
+	data := []int{1, 4, 2, 5, 3}
+	expected := []int{1, 2, 3, 4, 5}
+
+	actual := merge_sort(data)
+
+	for i, v := range actual {
+		if v != expected[i] {
+			t.Errorf("Expected %d, got %d", expected[i], v)
+		}
+	}
+}
+
+func TestLotsOfData(t *testing.T) {
+	data := make([]int, 10000)
+	for i := 0; i < 10000; i++ {
+		data[i] = rand.Int()
+	}
+
+	expected := make([]int, 10000)
+	copy(expected, data)
+	sort.Ints(expected)
+
+	actual := merge_sort(data)
+
+	for i, v := range actual {
+		if v != expected[i] {
+			t.Errorf("Expected %d, got %d", expected[i], v)
+		}
+	}
 }
