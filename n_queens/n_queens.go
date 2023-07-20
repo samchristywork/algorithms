cf7563bfe16348530757e090481a778eaadc9153
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-20 18:28:57 -0500

    feat: Add display function
    This function prints out the state of the board. I even added a cool
    border with special characters.

--- n_queens/n_queens.go
+++ n_queens/n_queens.go
@@ -21,6 +21,35 @@ func (board *Board)init_board(size int) {
 	}
 }
 
+func (board *Board)display() {
+	fmt.Print("┌")
+	for i := 0; i < board.Width*2; i++ {
+		fmt.Print("─")
+	}
+	fmt.Println("┐")
+
+	for i := 0; i < board.Height; i++ {
+		fmt.Print("│")
+		for j := 0; j < board.Width; j++ {
+			switch board.Board[i][j] {
+			case 1:
+				fmt.Print("Q ")
+			case 0:
+				fmt.Print(". ")
+			default:
+				fmt.Print("? ")
+			}
+		}
+		fmt.Println("│")
+	}
+
+	fmt.Print("└")
+	for i := 0; i < board.Width*2; i++ {
+		fmt.Print("─")
+	}
+	fmt.Println("┘")
+}
+
 func main() {
 	size := 8
 
